b474d9a71d15b5952f7e3609c1dbc634e0e311a0
thu <mingfei.net@gmail.com>
2016-12-06 16:31:41 +0800

    Update

--- src/IfTest.java
+++ src/IfTest.java
@@ -1,16 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by Administrator
  * 2016/12/6
  */
 public class IfTest {
+
+    // [90,100] A
+    // [80,90) B
+    // [60,80) C
+    // [0,60) D
     public static void main(String[] args) {
-        int grade = 50;
-        if (grade >= 60) {
-            System.out.println("passed...");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input grade: ");
+        int grade = scanner.nextInt();
+
+        if (grade >= 90 && grade <= 100) {
+            System.out.println("A");
+        } else if (grade >= 80) {
+            System.out.println("B");
+        } else if (grade >= 60) {
+            System.out.println("C");
+        } else if (grade >= 0) {
+            System.out.println("D");
         }
-        // 90-100 A
-        // 80-90 B
-        // 60-80 C
-        // 0-60 D
+
+        String level = "A";
+        // out [90,100]
     }
 }
