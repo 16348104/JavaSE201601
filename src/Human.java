f91da9c6ab561b3acccd28335c7bbe4bf219a207
thu <mingfei.net@gmail.com>
2016-12-08 09:17:03 +0800

    Update

--- src/Human.java
+++ src/Human.java
@@ -2,8 +2,22 @@
  * Created by Administrator
  * 2016/12/8
  */
-public class Human {
+// 类 = 域 + 方法
+// class = filed + method
+class Human {
 //    相同的特征或属性
+//    姓名
+//    年龄
+//    性别
+//    身高
+//    体重
+    String name;
+    int age;
+    char gender;
+    double height;
+    double weight;
 
 //    相同的行为或功能
+//    学习
+//    工作
 }
