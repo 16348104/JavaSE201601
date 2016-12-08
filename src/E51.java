7c8d96c411965175b892ad51bba9a584714ba86d
thu <mingfei.net@gmail.com>
2016-12-08 14:44:55 +0800

    Update

---
+++ src/E51.java
@@ -0,0 +1,36 @@
+import org.ietf.jgss.Oid;
+
+import java.util.Scanner;
+
+/**
+ * Created by Administrator
+ * 2016/12/8
+ */
+// 斐波那契数列：1，1，2, 3，5，8, 13, 21, 34,...，求f(n)
+    /*
+    递归调用 Recursive call
+            F(n) = 1 (n=1, n=2)
+            F(n) = F(n-1) + F(n-2) (n>2)
+     */
+public class E51 {
+
+//    void method() {
+//        method();
+//    }
+
+    private int fibonacci(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("input n: ");
+        int n = scanner.nextInt();
+
+        E51 e51 = new E51();
+        System.out.println("F(" + n + ") = " + e51.fibonacci(n));
+    }
+}
